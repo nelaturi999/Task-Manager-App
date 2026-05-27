5c2fd339644a939c89feae5b2d8191f8603c6522
Sasankaru Reddy <nelaturisasankarureddy@gmail.com>
2026-05-27 13:37:37 +0530

    Fix CORS issue

--- Task-Manager-App/backend/server.js
+++ Task-Manager-App/backend/server.js
@@ -10,8 +10,11 @@ const taskRoutes = require("./routes/taskRoutes");
 
 const app = express();
 
-app.use(cors());
-app.use(express.json());
+app.use(
+  cors({
+    origin: "*",
+  })
+);app.use(express.json());
 
 app.use("/api/auth", authRoutes);
 app.use("/api/tasks", taskRoutes);
